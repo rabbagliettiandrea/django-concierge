01debb434e9d2e24b1ef3a01b5053730d5d032b3
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2016-11-13 20:42:25 +0100

    Logout now works on POST

--- concierge/views.py
+++ concierge/views.py
@@ -3,7 +3,8 @@
 from __future__ import unicode_literals, division, absolute_import
 
 from django.conf import settings
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -18,3 +19,9 @@ class SignupView(generic.FormView):
         user = form.save()
         login(self.request, user)
         return super(SignupView, self).form_valid(form)
+
+
+class LogoutView(generic.TemplateView):
+    def post(self, request, *args, **kwargs):
+        logout(self.request)
+        return redirect(settings.LOGOUT_REDIRECT_URL)
